0620ca4f6ef19711dc18e11b49f32613610dc97c
antonsjoo <anton.sjoo@hotmail.se>
2021-09-29 11:48:06 +0200

    minor

--- flashback-to-speech/src/App.js
+++ flashback-to-speech/src/App.js
@@ -1,10 +1,11 @@
 import PlayPause from "./PlayPause";
 import URLfetcher from "./URLfetcher";
-import "./App.css";
+
 
 function App() {
   return (
     <div>
+    <h1 className="text-5xl text-center p-20 font-bold">Flashmouse Fetcher.</h1>
       <PlayPause />
       <URLfetcher />
     </div>
